b490025478f28a887240af5473e0e4f10ef2f901
Vinícius Sanches Geniselli <vsgeniselli@gmail.com>
2022-02-16 17:07:08 -0300

    Lógica para gerar um número aleatório a cada 5 segundos e salvar em um banco de dados, precisando ainda de melhoria, pois salva data nula e número 0

--- client/src/Views/Dashboard.js
+++ client/src/Views/Dashboard.js
@@ -1,11 +1,39 @@
-import React from 'react'
-import ApexChart from '../Components/ApexChart'
+import React, { useEffect, useState } from 'react'
+// import ApexChart from '../Components/ApexChart'
+import axios from 'axios'
 
 export default function Dashboard () {
+  const [number, setNumber] = useState(0)
+  const [horario, setHorario] = useState('')
+  let dataObj = new Date()
+  const hora = `${dataObj.getFullYear()}-${dataObj.getMonth() + 1}-${dataObj.getDate()} ${dataObj.getHours()}:${dataObj.getMinutes()}:${dataObj.getSeconds()}`
+
+  useEffect(() => {
+    setInterval(() => {
+      axios.get('https://random-data-api.com/api/number/random_number').then(
+        res => {
+          setNumber(res.data.decimal)
+          setHorario(horario)
+        })
+    }, 5000)
+  }, [])
+
+  console.log(number, hora)
+
+  useEffect(() => {
+    const dados = {
+      numero: number,
+      hora: hora
+    }
+    axios.post('http://localhost:3001/api', dados).then(res => {
+    })
+  }, [])
+
   return (
     <div>
       <h1>Dashboard</h1>
-      <ApexChart/>
+      {/* <ApexChart/> */}
+      {number}
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const cors = require('cors')
+const { Dados } = require("./database/models")
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+app.post('/api', async (req, res) => {
+  const dado = req.body;
+  await Dados.create(dado);
+  res.json(dado)
+})
 
 app.listen(3001, () => {
   console.log('Servidor rodando na porta 3001')
